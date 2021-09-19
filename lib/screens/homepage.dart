cb9204b31eabeffefe0ffc3a19681d1ba7c87602
Siddhant Mehta <siddhantmehta02@gmail.com>
2021-09-19 22:14:26 +0530

    homepage ui added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,15 +12,48 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.grey.shade800,
-        title: Text('Trading app'),
+        backgroundColor: Colors.black,
+        // title: Text('Trading app'),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.notifications)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+        ],
         centerTitle: true,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.grey.shade800,
-        child: Icon(Icons.edit),
+      body: Row(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Card(
+                child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: Image.asset('assets/images/coins.jpeg')),
+                      Text(
+                        'cash',
+                        style: TextStyle(color: Colors.white),
+                      )
+                    ],
+                  ),
+                ),
+                color: Colors.grey.shade900,
+              ),
+            ],
+          ),
+        ],
       ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {},
+      //   backgroundColor: Colors.grey.shade800,
+      //   child: Icon(Icons.edit),
+      // ),
     );
   }
 }
